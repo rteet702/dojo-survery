126739667a2dd75d2c2ed3c7e58ac6a19b6b8c61
Robert Teets <teetsrobert@gmail.com>
2022-08-24 22:32:03 -0600

    satisfied assignment requirements

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, session, request
 
 app = Flask(__name__)
 app.secret_key = 'tiamat is the best'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    session['form_data'] = request.form
+    return redirect('/result')
+
+
+@app.route('/result')
+def result():
+    return render_template('result.html', form_data=session['form_data'])
 
 if __name__ == '__main__':
     app.run(debug=True)
